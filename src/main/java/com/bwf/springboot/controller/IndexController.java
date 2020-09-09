67ee9ba354219d4ab4d8017bddd252f2f625bf33
titushan <titushan2010@163.com>
2020-09-09 11:45:01 +0800

    李四 提交于 2020/09/09 11:44

--- src/main/java/com/bwf/springboot/controller/IndexController.java
+++ src/main/java/com/bwf/springboot/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value={"/","/index"})
 public class IndexController {
+
     @RequestMapping(value={"/","/index"})
     @ResponseBody
     public String index(){
