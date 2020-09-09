0fb8806a3a8ef18bfc55153395d0a9848eb85d46
titushan <titushan2010@163.com>
2020-09-09 14:04:31 +0800

    张三 更新于 2020/09/09 14:04

---
+++ src/main/java/com/bwf/springboot/service/impl/UserService.java
@@ -0,0 +1,9 @@
+package com.bwf.springboot.service.impl;
+
+import com.bwf.springboot.service.IUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements IUserService {
+
+}
